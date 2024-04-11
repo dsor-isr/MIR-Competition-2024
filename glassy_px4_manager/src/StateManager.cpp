40ed5582dd259c5529325f1e179fa16b50ea3393
joaolehodey <joao.lehodey@gmail.com>
2024-04-11 14:57:32 +0100

    some changes, removing direct px4 dependency by creating a layer between ( this allows for example to have the state publisher using body velocities instead of ned, which is what we want)

--- glassy_px4_manager/src/StateManager.cpp
+++ glassy_px4_manager/src/StateManager.cpp
@@ -16,6 +16,13 @@ StateManager::StateManager() : Node("glassy_state_manager")
     this->declare_parameter("mission_type", 2);
     this->declare_parameter("types/PATH_FOLLOWING", 1);
     this->declare_parameter("types/SUMMER_SCHOOL_CHALLENGE", 2);
+    this->declare_parameter("rates/state_publishing", 50);
+    this->declare_parameter("rates/actuator_publishing", 40);
+
+
+    // get parameters
+    int state_publishing_rate = this->get_parameter("rates/state_publishing").as_int();
+    int actuator_publishing_rate = this->get_parameter("rates/actuator_publishing").as_int();
 
     // Get the parameters
     int mission = this->get_parameter("mission_type").as_int();
@@ -33,35 +40,107 @@ StateManager::StateManager() : Node("glassy_state_manager")
         mission_type_ = SUMMER_SCHOOL_CHALLENGE;
     }
 
+
+    // Initialize the variables
+    state_px4_msg_ = std::make_shared<glassy_msgs::msg::State>();
+    actuators_msg_ = std::make_shared<glassy_msgs::msg::Actuators>();
+    thrust_msg_ = std::make_shared<VehicleThrustSetpoint>();
+    torque_msg_ = std::make_shared<VehicleTorqueSetpoint>();
+
+    // Initialize publishers
+    state_px4_publisher_ = this->create_publisher<glassy_msgs::msg::State>("/glassy/state_px4", 10);
     offboard_control_mode_publisher_ = this->create_publisher<OffboardControlMode>("/fmu/in/offboard_control_mode", 10);
     thrust_setpoint_publisher_ = this->create_publisher<VehicleThrustSetpoint>("/fmu/in/vehicle_thrust_setpoint", 10);
     torque_setpoint_publisher_ = this->create_publisher<VehicleTorqueSetpoint>("/fmu/in/vehicle_torque_setpoint", 10);
     vehicle_command_publisher_ = this->create_publisher<VehicleCommand>("/fmu/in/vehicle_command", 10);
 
+    //subriber profile
     rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
     auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 5), qos_profile);
 
+    //Initialize subscribers
     vehicle_control_mode_ = this->create_subscription<VehicleControlMode>("fmu/out/vehicle_control_mode", qos, std::bind(&StateManager::vehicle_control_mode_callback, this, std::placeholders::_1));
+
+    vehicle_odometry_ = this->create_subscription<VehicleOdometry>("fmu/out/vehicle_odometry", qos, std::bind(&StateManager::vehicle_odometry_callback, this, std::placeholders::_1));
+
+    actuator_glassy_subscriber_ = this->create_subscription<glassy_msgs::msg::Actuators>("/glassy/actuators",1,  std::bind(&StateManager::actuator_glassy_callback, this, std::placeholders::_1));
 
     // define the clients for the services
     path_following_client_ = this->create_client<std_srvs::srv::SetBool>("/fmu/in/path_following");
     start_mission_summer_challenge_client_ = this->create_client<std_srvs::srv::SetBool>("start_stop_challenge");
 
+    // define timers for state publishing and actuator publishing
+    timer_actuator_publishing_ = this->create_wall_timer((1.0s/actuator_publishing_rate), std::bind(&StateManager::publish_offboard_actuator_signals, this));
 
-    timer_ = this->create_wall_timer(100ms, std::bind(&StateManager::publish_offboard_signals, this));
+    timer_state_publishing_ = this->create_wall_timer((1.0s/state_publishing_rate), std::bind(&StateManager::publish_state_callback, this));
 
 }
 
 
 
-// /**
-//  * @brief This takes the state of the vehicle from px4 and turns in into a more readable format. Easier to debug/addition of * body velocities/...
-//  * 
-//  * 
-//  */
-// void to_be_done(const VehicleOdometry::SharedPtr msg){
-//     //TODO
-// }
+/**
+ * @brief This takes the state of the vehicle from px4 and turns in into a more readable format. Easier to debug/addition of * body velocities/...
+ * The odometry message should be received at around 125 hz, so the state is updated at that rate.
+ */
+void StateManager::vehicle_odometry_callback(const VehicleOdometry::SharedPtr msg){
+
+    state_px4_msg_->header.stamp = this->get_clock()->now();
+
+    // populate an eigen quaternion with the info from the odometry msg 
+    Eigen::Quaterniond q(msg->q[0], msg->q[1], msg->q[2], msg->q[3]);
+
+    // convert the quaternion to euler angles
+    Eigen::Vector3d euler_angles= q.toRotationMatrix().eulerAngles(2, 1, 0);
+
+    state_px4_msg_->yaw = euler_angles[0];
+    state_px4_msg_->pitch = euler_angles[1];
+    state_px4_msg_->roll = euler_angles[2];
+
+
+    state_px4_msg_->yaw_rate = msg->angular_velocity[2];
+    state_px4_msg_->pitch_rate = msg->angular_velocity[1];
+    state_px4_msg_->roll_rate = msg->angular_velocity[0];
+
+
+
+    if(msg->pose_frame == VehicleOdometry::POSE_FRAME_NED){
+        state_px4_msg_->p_ned[0] = msg->position[0];
+        state_px4_msg_->p_ned[1] = msg->position[1];
+        state_px4_msg_->p_ned[2] = msg->position[2];
+    } else{
+        RCLCPP_WARN(this->get_logger(), "Unknown pose frame, not updating NED position");
+    }
+
+    if(msg->velocity_frame == VehicleOdometry::VELOCITY_FRAME_BODY_FRD){
+        // populate the body velocities
+        state_px4_msg_->v_body[0] = msg->velocity[0];
+        state_px4_msg_->v_body[1] = msg->velocity[1];
+        state_px4_msg_->v_body[2] = msg->velocity[2];
+    } else if (msg->velocity_frame == VehicleOdometry::VELOCITY_FRAME_NED){
+
+        // get the vector in ned, then transform it to body frame
+        Eigen::Vector3d v_ned = Eigen::Vector3d(msg->velocity[0], msg->velocity[1], msg->velocity[2]);
+        Eigen::Vector3d v_body = q.conjugate()._transformVector(v_ned);
+
+        // populate the body velocities
+        state_px4_msg_->v_body[0] = v_body[0];
+        state_px4_msg_->v_body[1] = v_body[1];
+        state_px4_msg_->v_body[2] = v_body[2];
+    }
+    else{
+        RCLCPP_WARN(this->get_logger(), "Unknown velocity frame, not updating body velocities");
+    }
+
+}
+
+/**
+ * @brief This callback is used to update the thrust and rudder values of the vehicle.
+ * @param msg   The actuators message containing the thrust and rudder values.
+ */
+void StateManager::actuator_glassy_callback(const glassy_msgs::msg::Actuators::SharedPtr msg){
+    thrust_ = msg->thrust;
+    rudder_ = msg->rudder;
+}
 
 
 /**
@@ -72,10 +151,6 @@ void StateManager::vehicle_control_mode_callback(const VehicleControlMode::Share
 {
     offboard_mode_ = msg->flag_control_offboard_enabled;
     is_armed_ = msg->flag_armed;
-
-    std::cout<< "is armed : "<< is_armed_<<std::endl;
-    std::cout<< "is offboard : "<< offboard_mode_<<std::endl;
-    std::cout<< "is mission : "<< mission_is_on_<<std::endl;
 
     if (offboard_mode_ && is_armed_ && !mission_is_on_)
     {
@@ -119,40 +194,7 @@ void StateManager::publish_offboard_control_mode()
 }
 
 
-/**
- * @brief Publish null thrust and torque setpoint.
- *  This ensures offboard mode can be entered from a Ground station.       
- */
-void StateManager::publish_offboard_signals()
-{   
-    
-    if(mission_is_on_){
-        std::cout<<"returning in timer callback"<<std::endl;
-        return;
-    }
-    // in case of offboard mode and armed, do not publish, as the vehicle will be controlled by the mission.
-    publish_offboard_control_mode();
 
-    // create and populate the thrust and torque setpoint messages
-    VehicleThrustSetpoint thrust_msg{};
-    VehicleTorqueSetpoint torque_msg{};
-    thrust_msg.xyz[0] = 0.0;
-    thrust_msg.xyz[1] = 0.0;
-    thrust_msg.xyz[2] = 0.0;
-    torque_msg.xyz[0] = 0.0;
-    torque_msg.xyz[1] = 0.0;
-    torque_msg.xyz[2] = 0.0;
-
-	thrust_msg.timestamp_sample = this->get_clock()->now().nanoseconds() / 1000;
-	torque_msg.timestamp_sample = this->get_clock()->now().nanoseconds() / 1000;
-
-    thrust_msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-    torque_msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-
-    // publish the messages
-    thrust_setpoint_publisher_->publish(thrust_msg);
-    torque_setpoint_publisher_->publish(torque_msg);
-}
 
 /**
  * @brief Publish vehicle commands
@@ -216,15 +258,17 @@ bool StateManager::start_stop_mission(bool start){
  * @param start     True to start, False to stop
  */
 bool StateManager::start_stop_path_following(bool start){
+    bool is_service_available = start_mission_summer_challenge_client_->wait_for_service(0.1s);
+    if (!is_service_available) {
+        RCLCPP_ERROR(this->get_logger(), "Service not available");
+        this->disarm();
+        return false;
+    }
+
     std_srvs::srv::SetBool::Request::SharedPtr request = std::make_shared<std_srvs::srv::SetBool::Request>();
     request->data = start;
     auto result = path_following_client_->async_send_request(request);
-    if(result.get()->success){
-        return true;
-    }
-    else{
-        return false;
-    }
+    return true;
 }
 
 /**
@@ -245,4 +289,50 @@ bool StateManager::start_stop_summer_challenge(bool start){
     auto result = start_mission_summer_challenge_client_->async_send_request(request);
 
     return true;
+}
+
+
+/* ------------------------------
+    Timer Callbacks
+-------------------------------*/
+
+
+/**
+ * @brief Publish null thrust and torque setpoint.
+ *  This ensures offboard mode can be entered from a Ground station.       
+ */
+void StateManager::publish_offboard_actuator_signals()
+{   
+    
+    if(!mission_is_on_){
+        thrust_ = 0.0;
+        rudder_ = 0.0;
+    }
+    // in case of offboard mode and armed, do not publish, as the vehicle will be controlled by the mission.
+    publish_offboard_control_mode();
+
+    // create and populate the thrust and torque setpoint messages
+    thrust_msg_->xyz[0] = thrust_;
+    thrust_msg_->xyz[1] = 0.0;
+    thrust_msg_->xyz[2] = 0.0;
+    torque_msg_->xyz[0] = 0.0;
+    torque_msg_->xyz[1] = 0.0;
+    torque_msg_->xyz[2] = rudder_;
+
+	thrust_msg_->timestamp_sample = this->get_clock()->now().nanoseconds() / 1000;
+	torque_msg_->timestamp_sample = this->get_clock()->now().nanoseconds() / 1000;
+
+    thrust_msg_->timestamp = this->get_clock()->now().nanoseconds() / 1000;
+    torque_msg_->timestamp = this->get_clock()->now().nanoseconds() / 1000;
+
+    // publish the messages
+    thrust_setpoint_publisher_->publish(*thrust_msg_);
+    torque_setpoint_publisher_->publish(*torque_msg_);
+}
+
+/**
+ * @brief Publish the state of the vehicle in a compatible format to the glassy code stack (layer of independence to the px4 stack)
+ */
+void StateManager::publish_state_callback(){
+    state_px4_publisher_->publish(*state_px4_msg_);
 }
--- glassy_px4_manager/src/StateManager.h
+++ glassy_px4_manager/src/StateManager.h
@@ -17,11 +17,15 @@ Developers: João Lehodey - joao.lehodey@tecnico.ulisboa.pt - DSOR/ISR team (Ins
 #include <px4_msgs/msg/actuator_motors.hpp> 
 #include <px4_msgs/msg/actuator_servos.hpp>
 #include <px4_msgs/msg/vehicle_status.hpp>
+#include <px4_msgs/msg/vehicle_odometry.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <stdint.h>
 #include "mission_types.h"
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Geometry>
 
 #include <glassy_msgs/msg/state.hpp>
+#include <glassy_msgs/msg/actuators.hpp>
 
 #include <chrono>
 #include <iostream>
@@ -32,20 +36,31 @@ using namespace px4_msgs::msg;
 
 
 /**
-* @brief StateManager class
-* This class is responsible for managing the state of the ASV. It publishes offboard control mode and setpoints, allowing the User to * arm and enter offboard mode while using a ground control station. 
+* @brief StateManager class:
+* is responsible for managing the state of the ASV. It publishes offboard control mode and setpoints, allowing the User to * arm and enter offboard mode while using a ground control station. 
 * It detects the vehicle status, and if the vehicle is armed, and in offboard mode, it will initialize a mission, which will provide * the necessary offboard setopints to the vehicle.
 */
 class StateManager : public rclcpp::Node
 {
 public:
 	StateManager();
-
-    void vehicle_control_mode_callback(const VehicleControlMode::SharedPtr msg);
 	void disarm();
 
 private:
-	rclcpp::TimerBase::SharedPtr timer_;
+
+	/*
+		Timer
+	*/
+	rclcpp::TimerBase::SharedPtr timer_actuator_publishing_;
+	rclcpp::TimerBase::SharedPtr timer_state_publishing_;
+
+	/*
+		Timer callbacks
+	*/
+	void publish_state_callback();
+	void publish_offboard_control_mode();
+    void publish_offboard_actuator_signals();
+
 
     /*
         Publishers 
@@ -54,12 +69,23 @@ private:
 	rclcpp::Publisher<VehicleThrustSetpoint>::SharedPtr thrust_setpoint_publisher_;
 	rclcpp::Publisher<VehicleTorqueSetpoint>::SharedPtr torque_setpoint_publisher_;
 	rclcpp::Publisher<VehicleCommand>::SharedPtr vehicle_command_publisher_;
+	rclcpp::Publisher<glassy_msgs::msg::State>::SharedPtr state_px4_publisher_;
 
 
     /*
         Subscribers
     */
     rclcpp::Subscription<VehicleControlMode>::SharedPtr vehicle_control_mode_;
+	rclcpp::Subscription<VehicleOdometry>::SharedPtr vehicle_odometry_;
+	rclcpp::Subscription<glassy_msgs::msg::Actuators>::SharedPtr actuator_glassy_subscriber_;
+
+	/*
+		Subscriber callbacks	
+	*/
+    void vehicle_control_mode_callback(const VehicleControlMode::SharedPtr msg);
+	void vehicle_odometry_callback(const VehicleOdometry::SharedPtr msg);
+	void actuator_glassy_callback(const glassy_msgs::msg::Actuators::SharedPtr msg);
+
 
 	/*
 		Clients
@@ -69,15 +95,39 @@ private:
 
 	std::atomic<uint64_t> timestamp_;   //!< common synced timestamped
 
+
+
+	/*
+		Publishing msgs structure variables
+	*/
+	std::shared_ptr<glassy_msgs::msg::State> state_px4_msg_;
+	std::shared_ptr<glassy_msgs::msg::Actuators> actuators_msg_;
+	std::shared_ptr<VehicleThrustSetpoint> thrust_msg_;
+    std::shared_ptr<VehicleTorqueSetpoint> torque_msg_;
+
+	
+
+
+
+
+
 	MissionType mission_type_ = MissionType::SUMMER_SCHOOL_CHALLENGE;
+
+
+
+
+
+	// Actuator variables
+	float thrust_ = 0.0;
+	float rudder_ = 0.0;
+
+
+
 
     bool offboard_mode_=false;
     bool is_armed_=false;  
 	bool mission_is_on_=false;
 
-	void publish_offboard_control_mode();
-
-    void publish_offboard_signals();
 	void publish_vehicle_command(uint16_t command, float param1 = 0.0, float param2 = 0.0);
 
     bool start_mission();
